27ba14e8dbbfee16cf096f1d1af005f28df2563d
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-06-24 18:58:23 +0100

    alterar directoria de input

--- main.c
+++ main.c
@@ -6,19 +6,21 @@
 #include <conio.h>
 
 int main(){
-    const char* origem = "Ficheiros\\Participantes.txt";
+    const char* origem = "Ficheiros\\teste.txt";
     FILE* fp = le_f(origem);
 
     printf(" O Andre e o Duarte ja dominam o GITHUB \n");
 
     LinkedList *l = create();
     input_lista(origem, l);
+    print_list(l);
     remove_first(l);
     print_list(l);
     remove_last(l);
     print_list(l);
     //menu(l);
-
+    free(fp);
+    destroy(l);
     return 0;
 } //main
 
