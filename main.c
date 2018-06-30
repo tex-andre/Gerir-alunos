61e4dc9435d1157d672eb3ae694752704c4c20ad
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-06-30 15:46:38 +0100

    fase final

--- main.c
+++ main.c
@@ -10,8 +10,6 @@
 
 int main(){
     const char* origem = "Ficheiros\\Participantes.txt";
-    const char* teste_origem = "Ficheiros\\teste.txt";
-    const char* teste_destino = "Ficheiros\\teste.dat";
     const char* list_file = "Ficheiros\\Participantes.dat";
     float media = 0;
     //printf(" O Andre e o Duarte ja dominam o GITHUB \n");
@@ -19,16 +17,13 @@ int main(){
     LinkedList* l = create();
     FILE* fp = le_f(origem);
 
-    //input_list(fp, l);
+    input_list(fp, l);
     /********Para editar um aluno pela solução adotada pelo Professor*******/
 
-    inport_list_bin(teste_destino, l);
-
-    print_list(l);
-    menu(l, teste_destino);
+    menu(l, list_file);
 
     //export_list(l);
-    //fclose(fp);
+    fclose(fp);
     destroy(l);
     return 0;
 } //main
