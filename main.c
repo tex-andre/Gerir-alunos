e906ce7a0e92cf837b92d0793fb56d8952825d4d
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-06-30 10:46:38 +0100

    pequena correção

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int main(){
     printf("%f\n", media_final(l));
     media = media_final(l);
     printf("%f", media);
-    //export_lista(l);
+    //export_list(l);
     //fclose(fp);
     destroy(l);
     return 0;
