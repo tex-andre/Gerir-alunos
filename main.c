72aacaa973900f3cde45074fa4e5238124320896
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-06-30 23:43:50 +0100

    Final... por agora

--- main.c
+++ main.c
@@ -5,11 +5,7 @@
 #include "FileIO.h"
 #include <conio.h>
 
-
-
-
 int main(){
-    const char* origem = "Ficheiros\\Participantes.txt";
     const char* list_file = "Ficheiros\\Participantes.dat";
 
     printf("%s", asctime(data_atual()));
@@ -17,10 +13,7 @@ int main(){
     LinkedList* l = create();
     import_list_bin(list_file, l);
 
-
     menu(l, list_file);
-
-    //export_list(l);
 
     destroy(l);
     return 0;
