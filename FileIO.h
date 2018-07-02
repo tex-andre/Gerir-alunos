e0d06fb48a3fe6351589f7a337f2788cfdadaa54
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-07-02 11:57:26 +0100

    correções

--- FileIO.h
+++ FileIO.h
@@ -7,5 +7,5 @@ FILE* le_f (const char* local);
 FILE* escreve_fB(const char* local);
 FILE* le_fB(const char *local);
 FILE* cria_ponteiros (const char* local);
-
+int copia_ficheiro(const char* origem, const char* destino);
 #endif // FILEIO_H_INCLUDED
